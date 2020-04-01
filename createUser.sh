ce8e5369f20e4727723564b5534ca953b10cc7c1
Jon Wallem Anundsen <j.w.anundsen@protonmail.com>
2020-04-01 14:18:14 +0200

    chore(removal): remove createUser.sh

--- createUser.sh
+++
@@ -1,3 +0,0 @@
-echo "Creating users..."
-mongo admin --host wizard_test_mongo -u root -p secret --eval "db.createUser({user: 'test_user', pwd: 'test_password',roles: [{role: 'readWrite', db: 'wizard'}]});"
-echo "Users created."

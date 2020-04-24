c507ca260281408206e3ab61d7bb0fd2661ab8b7
Jon Wallem Anundsen <j.w.anundsen@protonmail.com>
2020-04-24 14:13:58 +0200

    test: make CSVcreatorTest work on all platforms

--- src/test/java/no/digipat/wizard/models/CSVcreatorTest.java
+++ src/test/java/no/digipat/wizard/models/CSVcreatorTest.java
@@ -4,11 +4,18 @@ import no.digipat.wizard.models.results.AnalysisComponent;
 import no.digipat.wizard.models.results.AnalysisResult;
 import no.digipat.wizard.models.results.AnalysisValue;
 import no.digipat.wizard.models.results.Results;
+
+import org.junit.ClassRule;
 import org.junit.Test;
+import org.junit.rules.TemporaryFolder;
 
 import java.util.ArrayList;
 
 public class CSVcreatorTest {
+    
+    @ClassRule
+    public static final TemporaryFolder folder = new TemporaryFolder();
+    
     private Results createResults(String analysisType) {
         AnalysisValue value1 = new AnalysisValue().setName("mean").setVal(-0.333f);
         AnalysisValue value2 = new AnalysisValue().setName("std").setVal(-0.333f);
@@ -22,7 +29,10 @@ public class CSVcreatorTest {
     @Test
     public void createCSVfromResultsTest() throws Exception {
         String str;
-        str = CSVcreator.toCSV(new ArrayList<Results>(){{add(createResults("he")); add(createResults("AB"));}}, "D:/", "he");
-        // not throw error, then everything went as planned
+        str = CSVcreator.toCSV(new ArrayList<Results>(){{
+            add(createResults("he"));
+            add(createResults("AB"));
+        }}, folder.getRoot().getAbsolutePath(), "he");
+        // If an exception is not thrown, then everything went as planned
     }
 }

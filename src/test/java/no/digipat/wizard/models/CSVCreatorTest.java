10adfe00f3310a7cf264dd5e6620f3757b5d610f
Jon Wallem Anundsen <j.w.anundsen@protonmail.com>
2020-05-07 01:13:55 +0200

    test: improve CSVCreatorTest

--- src/test/java/no/digipat/wizard/models/CSVCreatorTest.java
+++ src/test/java/no/digipat/wizard/models/CSVCreatorTest.java
@@ -8,9 +8,16 @@ import no.digipat.wizard.models.results.Results;
 import org.junit.ClassRule;
 import org.junit.Test;
 import org.junit.rules.TemporaryFolder;
+import org.junit.runner.RunWith;
+
+import junitparams.JUnitParamsRunner;
+import junitparams.Parameters;
 
 import java.util.ArrayList;
 
+import static org.junit.Assert.*;
+
+@RunWith(JUnitParamsRunner.class)
 public class CSVCreatorTest {
     
     @ClassRule
@@ -50,9 +57,9 @@ public class CSVCreatorTest {
     }
 
     @Test
-    public void createCSVfromResultsTest() throws Exception {
-        String str;
-        str = CSVCreator.toCSV(
+    @Parameters(method = "getCsvParameters")
+    public void createCSVfromResultsTest(String analyzeType, String expectedCsv) throws Exception {
+        String actualCsv = CSVCreator.toCSV(
                 new ArrayList<Results>() {
                     {
                         add(createResults("he"));
@@ -60,8 +67,28 @@ public class CSVCreatorTest {
                     }
                 },
                 folder.getRoot().getAbsolutePath(),
-                "he"
+                analyzeType
         );
-        // If an exception is not thrown, then everything went as planned
+        assertEquals(expectedCsv, actualCsv);
     }
+    
+    private static String[][] getCsvParameters() {
+        return new String[][] {
+            {"he", "YW5ub3RhdGlvbklkLHJlc3VsdHNfbmFtZSxyZXN1bHRzX2NvbXBvbmVudHNfbmFtZ"
+                    + "SxyZXN1bHRzX2NvbXBvbmVudHNfY29tcG9uZW50c19uYW1lLHJlc3Vsd"
+                    + "HNfY29tcG9uZW50c19jb21wb25lbnRzX3ZhbAozLjAsImhlIiwiSCIsI"
+                    + "m1lYW4iLC0wLjMzMwozLjAsImhlIiwiSCIsInN0ZCIsLTAuMzMzCjMuM"
+                    + "CwiaGUiLCJFIiwibWVhbiIsLTAuMzMzCjMuMCwiaGUiLCJFIiwic3RkIiwtMC4zMzMK"
+            },
+            {null, "YW5ub3RhdGlvbklkLHJlc3VsdHNfbmFtZSxyZXN1bHRzX2NvbXBvbmVudHN"
+                    + "fbmFtZSxyZXN1bHRzX2NvbXBvbmVudHNfY29tcG9uZW50c19uYW1lLHJ"
+                    + "lc3VsdHNfY29tcG9uZW50c19jb21wb25lbnRzX3ZhbAozLjAsImhlIiw"
+                    + "iSCIsIm1lYW4iLC0wLjMzMwozLjAsImhlIiwiSCIsInN0ZCIsLTAuMzM"
+                    + "zCjMuMCwiaGUiLCJFIiwibWVhbiIsLTAuMzMzCjMuMCwiaGUiLCJFIiw"
+                    + "ic3RkIiwtMC4zMzMKMy4wLCJBQiIsIkgiLCJtZWFuIiwtMC4zMzMKMy4"
+                    + "wLCJBQiIsIkgiLCJzdGQiLC0wLjMzMwozLjAsIkFCIiwiRSIsIm1lYW4"
+                    + "iLC0wLjMzMwozLjAsIkFCIiwiRSIsInN0ZCIsLTAuMzMzCg=="}
+        };
+    }
+    
 }
